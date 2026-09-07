4dfd514819f9d6561dce7413cac68d2280d0e02e
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2026-09-07 18:38:55 -0400

    init

---
+++ src/main/java/com/eclectusstudio/pantheonYAML/PantheonYAML.java
@@ -0,0 +1,16 @@
+package com.eclectusstudio.pantheonYAML;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PantheonYAML extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/eclectusstudio/pantheonYAML/PantheonYAMLBootstrap.java
@@ -0,0 +1,12 @@
+package com.eclectusstudio.pantheonYAML;
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+
+class PantheonYAMLBootstrap implements PluginBootstrap {
+
+    @Override
+    public void bootstrap(final BootstrapContext context) {
+        // Plugin bootstrap logic
+    }
+}
